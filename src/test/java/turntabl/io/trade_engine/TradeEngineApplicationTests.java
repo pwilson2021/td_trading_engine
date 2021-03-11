79e020a39e9a8f3f6934684a5ca27fbcb7a25288
pwilson2021 <philip.yirenkyi@turntabl.io>
2021-03-11 19:26:20 +0000

    setup gradle

---
+++ src/test/java/turntabl/io/trade_engine/TradeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package turntabl.io.trade_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
